c6414e6621b7a80b4d532ddf17e235a85e6ead54
aabhasmathur <aabhasmathur01@gmail.com>
2025-12-16 12:18:11 +0530

    image component

--- blocks/_image_custom/image_custom.js
+++ blocks/_image_custom/image_custom.js
@@ -1,22 +1,28 @@
 export default function decorate(block) {
-  const { aspectRatio, sizePreset } = block.dataset;
+  const {
+    image,
+    imageAlt,
+    aspectRatio,
+    sizePreset,
+  } = block.dataset;
 
+  // Add classes for styling
   if (aspectRatio) {
     block.classList.add(`aspect-${aspectRatio}`);
   }
-
   if (sizePreset) {
     block.classList.add(`size-${sizePreset}`);
   }
 
-  // Remove width and height attributes from img to allow CSS aspect-ratio to work
-  const img = block.querySelector('img');
-  if (img) {
-    img.removeAttribute('width');
-    img.removeAttribute('height');
-    // Set alt text if provided
-    if (block.dataset.imageAlt) {
-      img.setAttribute('alt', block.dataset.imageAlt);
-    }
+  // Create the picture element if image is provided
+  if (image) {
+    const picture = document.createElement('picture');
+    const img = document.createElement('img');
+    img.src = image;
+    img.alt = imageAlt || '';
+    img.loading = 'lazy';
+    // Remove any default width/height to let CSS control aspect ratio
+    picture.appendChild(img);
+    block.appendChild(picture);
   }
 }
