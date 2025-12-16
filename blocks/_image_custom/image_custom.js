4d88029d934594c8866c614e4eba6ae1eda4bfe1
aabhasmathur <aabhasmathur01@gmail.com>
2025-12-16 08:21:21 +0530

    image component

--- blocks/_image_custom/image_custom.js
+++ blocks/_image_custom/image_custom.js
@@ -1,6 +1,18 @@
 export default function decorate(block) {
-  const { color } = block.dataset;
-  if (color) {
-    block.classList.add(color);
+  const { aspectRatio, sizePreset } = block.dataset;
+
+  if (aspectRatio) {
+    block.classList.add(`aspect-${aspectRatio}`);
+  }
+
+  if (sizePreset) {
+    block.classList.add(`size-${sizePreset}`);
+  }
+
+  // Optional: If you need to decorate the image element further, add logic here
+  // For example, to set alt text or apply to the img tag
+  const img = block.querySelector('img');
+  if (img && block.dataset.imageAlt) {
+    img.setAttribute('alt', block.dataset.imageAlt);
   }
 }
