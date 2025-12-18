c177e1276969af82f45c37387c8cbfb2c1ae0e2a
aabhasmathur <aabhasmathur01@gmail.com>
2025-12-18 16:15:30 +0530

    image component

--- scripts/aem.js
+++ scripts/aem.js
@@ -499,12 +499,40 @@ function decorateSections(main) {
     if (sectionMeta) {
       const meta = readBlockConfig(sectionMeta);
       Object.keys(meta).forEach((key) => {
-        if (key === 'style-color') {
-          const styles = meta['style-color']
-            .split(',')
-            .filter((style) => style)
-            .map((style) => toClassName(style.trim()));
-          styles.forEach((style) => section.classList.add(style));
+        if (key === 'style-color' || key === 'style-width' || key === 'style-layout') {
+//added code
+const styles = [
+  meta['style-color'],
+  meta['style-width'],
+  meta['style-layout']
+]
+  .filter(Boolean)
+  .flatMap((value) =>  value
+      .split(',')
+      .map((style) => toClassName(style.trim()))
+  );
+
+styles.forEach((style) => section.classList.add(style));
+
+
+
+
+
+
+
+
+
+
+
+
+          // const styles = meta['style-color']
+          //   .split(',')
+          //   .filter((style) => style)
+          //   .map((style) => toClassName(style.trim()));
+          // styles.forEach((style) => section.classList.add(style));
+
+
+
         } else {
           section.dataset[toCamelCase(key)] = meta[key];
         }
